3ee4a8da79fd3725e7bb7d7b2a809b44098f9c50
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:35:32 +0100

    Spring Boot Project Initial

---
+++ Core_Role/src/main/java/de/hska/iwi/vslab/Core_Role/CoreRoleApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_Role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreRoleApplication.class, args);
+	}
+
+}
---
+++ Core_Role/src/test/java/de/hska/iwi/vslab/Core_Role/CoreRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Core_Role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
