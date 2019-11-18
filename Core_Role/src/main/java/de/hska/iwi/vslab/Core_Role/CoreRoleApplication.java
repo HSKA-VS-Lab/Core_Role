56b02ae7443e76701c5cacfd864693ceec6b0de7
and1kl <email.andreasklaiber@gmail.com>
2019-11-18 22:30:18 +0100

    Eureka Registrierung

--- Core_Role/src/main/java/de/hska/iwi/vslab/Core_Role/CoreRoleApplication.java
+++ Core_Role/src/main/java/de/hska/iwi/vslab/Core_Role/CoreRoleApplication.java
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CoreRoleApplication {
 	private static final Logger log = LoggerFactory.getLogger(CoreRoleApplication.class);
